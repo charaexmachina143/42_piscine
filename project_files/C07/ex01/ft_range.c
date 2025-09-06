054539cc4b309a867a9864e62f89981235840388
Chara Oberste-Lehn <doberste@student.42vienna.com>
2025-09-06 14:33:08 +0200

    norminette fix

--- project_files/C07/ex01/ft_range.c
+++ project_files/C07/ex01/ft_range.c
@@ -6,30 +6,35 @@
 /*   By: doberste <doberste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 13:18:28 by doberste          #+#    #+#             */
-/*   Updated: 2025/09/06 14:29:58 by doberste         ###   ########.fr       */
+/*   Updated: 2025/09/06 14:33:00 by doberste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-int *ft_range(int min, int max);
 
-int *ft_range(int min, int max)
+int	*ft_range(int min, int max);
+
+int	*ft_range(int min, int max)
 {
-    int *range = malloc(max - min);
-    int o = min;
-    int i = 0; 
-    while(o < max)
-    {
-        range[i] = o;
-        i++;
-        o++;
-    }
-    if(min > max || (max == 0 && min == 0))
-    {
-        return NULL;
-    }
-    return range;
+	int	*range;
+	int	o;
+	int	i;
+
+	range = malloc(max - min);
+	o = min;
+	i = 0;
+	while (o < max)
+	{
+		range[i] = o;
+		i++;
+		o++;
+	}
+	if (min > max || (max == 0 && min == 0))
+	{
+		return (NULL);
+	}
+	return (range);
 }
 
 // int main()
@@ -38,7 +43,7 @@ int *ft_range(int min, int max)
 //     int max = 2147483647;
 //     long long ic = max - min;
 //     int *range = ft_range(min, max);
-//     int i = 0; 
+//     int i = 0;
 
 //     while(i < ic)
 //     {
