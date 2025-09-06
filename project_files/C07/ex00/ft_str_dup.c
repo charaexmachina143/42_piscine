a3254ef8fc5b102d66a1e62bc573721d160fd088
Chara Oberste-Lehn <doberste@student.42vienna.com>
2025-09-06 13:11:04 +0200

    c07 ex00 added

---
+++ project_files/C07/ex00/ft_str_dup.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_dup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doberste <doberste@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/06 12:43:18 by doberste          #+#    #+#             */
+/*   Updated: 2025/09/06 13:11:03 by doberste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_strdup(char *src);
+int		ft_strlen(char *str);
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	dest = malloc(ft_strlen(src) + 1);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+// int	main(void)
+// {
+// 	char	*result;
+
+// 	result = ft_strdup("Chara");
+// 	printf("%s", result);
+// 	free(result);
+// }
